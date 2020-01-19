b5596580bed5f362c04e7d752408a81305459c59
João Carreira de Oliveira e Mello <joao.mello@nexaas.com>
2020-01-19 15:02:20 -0300

    Create BankTransfer validation for source account amount

--- app/models/bank_transfer.rb
+++ app/models/bank_transfer.rb
@@ -3,4 +3,16 @@ class BankTransfer < ApplicationRecord
   belongs_to :destination_account, class_name: "Account"
 
   validates :source_account_id, :destination_account_id, :amount, presence: true
+
+  validate :source_account_amount
+
+  private
+
+  def source_account_amount
+    return if source_account.blank?
+    return if (source_account.balance - amount) >= 0
+    message = "does not have enough amount"
+    errors.add(:source_account, message)
+    errors.add(:source_account_id, message)
+  end
 end
--- spec/models/bank_transfer_spec.rb
+++ spec/models/bank_transfer_spec.rb
@@ -10,5 +10,43 @@ RSpec.describe BankTransfer, type: :model do
     it { is_expected.to validate_presence_of(:source_account_id) }
     it { is_expected.to validate_presence_of(:destination_account_id) }
     it { is_expected.to validate_presence_of(:amount) }
+
+    describe "source_account_amount" do
+      let!(:source_account) { FactoryBot.create(:account, initial_amount: 3000) }
+      let!(:destination_account) { FactoryBot.create(:account, initial_amount: 0) }
+
+      context "when source account has the amount to transfer to the destination account" do
+        subject { described_class.new(source_account_id: source_account.id, destination_account_id: destination_account.id, amount: 1000) }
+
+        it { is_expected.to be_valid }
+
+        it "does not add errors" do
+          subject.valid?
+          expect(subject.errors).to be_empty
+        end
+      end
+
+      context "when source account has exactly the amount for the transfer" do
+        subject { described_class.new(source_account_id: source_account.id, destination_account_id: destination_account.id, amount: 3000) }
+
+        it { is_expected.to be_valid }
+
+        it "does not add errors" do
+          subject.valid?
+          expect(subject.errors).to be_empty
+        end
+      end
+
+      context "when source account does not enough amount to transfer to the destination account" do
+        subject { described_class.new(source_account_id: source_account.id, destination_account_id: destination_account.id, amount: 4000) }
+
+        it { is_expected.to be_invalid }
+
+        it "adds errors" do
+          subject.valid?
+          expect(subject.errors["source_account_id"]).to include("does not have enough amount")
+        end
+      end
+    end
   end
 end
